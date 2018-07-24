ca12a2a8861947d24c3c8e56db0a61ac4fe646f2
Florian Merz <flomerz@gmail.com>
2018-07-24 12:46:03 +0200

    First version of the profile publisher.

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-import express from "express";
-import bodyParser from "body-parser";
 import { load, logger } from "./lib/config";
+import App from "./lib/app";
+
+let app;
 
 async function main() {
   try {
-    const app = express();
-    app.use(bodyParser.json());
-    const cfg = await load();
+    const cfg = await load("config.json");
 
-    app.listen(cfg.port, () => undefined);
+    app = new App(cfg);
+
+    logger.info(`starting to serve on port: ${cfg.port}`);
+    app.run();
   } catch (e) {
     logger.error(`Something went batshit wrong: ${e}`);
     process.exit(1);
   }
 }
+
+async function shutdown() {
+  try {
+    logger.info("shutting down…");
+    await app.stop();
+    logger.info("shut down gracefully :)");
+    process.exit(0);
+  } catch (e) {
+    logger.error(`faild to stop: ${e}`);
+    logger.error("exiting with exit code 1 :/");
+    process.exit(1);
+  }
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
 
 main();
